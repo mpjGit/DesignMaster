08778bd289c54b7dd4109ef14e45d9ae8692fac3
CF <422081094@qq.com>
2019-09-07 21:11:09 +0800

    项目引入 ajax

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import axios from 'axios';
 import App from './App';
 import router from './router';
 import './mock/index';
@@ -16,3 +17,4 @@ new Vue({
   components: { App },
   template: '<App/>',
 });
+Vue.prototype.$ajax = axios;
