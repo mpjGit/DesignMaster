5ed3b0eba946129c6b02d29f72180418a3597d3e
DellZhou <422081094@qq.com>
2019-09-09 23:49:25 +0800

    项目中引入 element-ui 提高开发效率

--- src/components/comHeader.vue
+++ src/components/comHeader.vue
@@ -1,5 +1,12 @@
 <template>
-    
+    <div class="container">
+        <span class="logo"></span>
+        <div class="button-contain">
+            <!-- <span class="commonButton" v-for></span>  -->
+            <span class="register"></span>
+            <span class="register"></span>
+        </div>
+    </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import axios from 'axios';
+import elementUi from 'element-ui';
 import App from './App';
 import router from './router';
 import './mock/index';
 import store from './store/store';
 
 Vue.config.productionTip = false;
+Vue.use(elementUi);
 
 /* eslint-disable no-new */
 new Vue({
