17d4de90b2aa13da89c6d4f7e9eea969f45da496
DellZhou <422081094@qq.com>
2019-09-10 23:49:15 +0800

    调试一下 element-ui

--- src/components/comHeader.vue
+++ src/components/comHeader.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="container">
-    <span class="logo"></span>
-    <div class="button-contain">
-      <span class="commonButton" v-for="item in buttons" :key="item.name">
-        {{ item.name }}
-      </span>
-      <span class="register"></span>
-      <span class="register"></span>
-    </div>
+    <el-row>
+      <el-col :span="4"><div class='logo-content'></div></el-col>
+      <el-col :span="12"><div class="midCross"></div></el-col>
+      <el-col :span="8">
+        <div class="button-content">
+           
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -23,5 +24,18 @@ export default {
 </script>
 
 <style lang="stylus">
-
+  .logo-content
+    background #99a9bf
+  .midCross
+    background #d3dce6
+  .button-content
+    background #e5e9f2
+  .el-row
+    margin-bottom 20px
+    div
+      min-height 36px
+  &:last-child
+    margin-bottom 0
+  .el-col
+    border-radius 4px
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 import elementUi from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
 import router from './router';
 import './mock/index';
